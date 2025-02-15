babadc1f2152ee2ed6d428f70081c99e2b195b11
hadan24 <ha.dan.t02@gmail.com>
2025-02-15 13:34:27 -0800

    started Cherno tutorial for more insights. Can specify size of new vec for realloc

--- src/Vector.h
+++ src/Vector.h
@@ -1,7 +1,14 @@
 #pragma once
 #include <iostream>
 
-constexpr size_t DEFAULT_INIT_SIZE = 1;
+/*
+    Everything done on 2/15/25 follows The Cherno's tutorial to start:
+    https://youtu.be/ryRf4Jh_YC0?si=QJgzHvqrqvKwkWyv
+*/
+
+
+constexpr static size_t INIT_SIZE = 1;
+constexpr static size_t GROW_FACTOR = 2;
 
 template<typename T>
 class Vector
@@ -25,12 +32,10 @@ public:
 
     /* DATA OPERATIONS */
     void push(const T& item) {
-        if (!m_data) {
-            m_data = new T[DEFAULT_INIT_SIZE];
-            m_cap = DEFAULT_INIT_SIZE;
-        }
-        else if (m_len == m_cap)
-            m_data = this->realloc();
+        if (!m_data)
+            this->realloc(INIT_SIZE);
+        else if (m_len >= m_cap)
+            this->realloc(m_cap * GROW_FACTOR);
 
         m_data[m_len] = item;
         m_len++;
@@ -42,6 +47,26 @@ public:
     }
 
     /* OTHER UTILITIES */
+    /*
+    pop should call obj destructor
+
+    emplace (placement new()???)
+    iterators
+    insert (index & iter versions)
+    erase (index & iter versions)
+    print
+    fill
+    swap
+    empty
+    reserve
+    shrink_to_fit
+    front, back
+    negative indices
+    resize
+    alternate constructors
+    copy & move constructors
+    comparison operators
+    */
 
 
     /* OPERATORS */
@@ -70,14 +95,18 @@ private:
     size_t m_len = 0;
     size_t m_cap = 0;
 
-    T* realloc() {
-        T* temp = m_data;
-        m_cap *= 2;
+    void realloc(size_t new_cap) {
+        T* old = m_data;
+
+        if (new_cap < m_len)
+            m_len = m_cap = new_cap;
+        else
+            m_cap = new_cap;
         m_data = new T[m_cap];
 
         for (size_t i = 0; i < m_len; i++)
-            m_data[i] = temp[i];
-        delete[] temp;
-        return m_data;
+            m_data[i] = std::move(old[i]);
+        delete[] old;
+        return;
     }
 };
